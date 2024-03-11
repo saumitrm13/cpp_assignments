94b6ef0fd9f5560135348cb5b51b946b337e6272
sunbeam <mulaysaumitr@gmail.com>
2024-03-11 21:44:53 +0530

    assg 3 q 2

--- Assignment_3/stack.cpp
+++ Assignment_3/stack.cpp
@@ -1,18 +1,95 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-class Stack{
-    int top;
-    int *arr[];
-    int stacksize;
-    
+class Stack {
+private:
+    int* arr;
+    int top;  
+    int size; 
 
-    Stack():Stack(5){}
-
-    Stack(int size):top(-1),stacksize(size),arr(arr[size]){
-        
-        cout<<"Stack Created\n";
-
+public:
+    Stack(int stackSize = 5) {
+        size = stackSize;
+        arr = new int[size];
+        top = -1; 
     }
+
+    ~Stack() {
+        delete[] arr;
+    }
+
+   
+    void push(int value) {
+        if (isFull()) {
+            cout << "Stack overflow. Cannot push element.\n";
+            return;
+        }
+        arr[++top] = value;
+        cout << "Added " << value << " onto the stack.\n";
+    }
+
+    
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack underflow. Cannot pop element.\n";
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    
+    int peek() {
+        if (isEmpty()) {
+            cout << "Stack is empty. Cannot peek.\n";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    bool isFull() {
+        return top == size - 1;
+    }
+
+    void print() {
+        if (isEmpty()) {
+            cout << "Stack is empty.\n";
+            return;
+        }
+        cout << "Stack elements: ";
+        for (int i = top; i >= 0; i--) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    
+    Stack myStack;
+
+    
+    myStack.push(10);
+    myStack.push(20);
+    myStack.push(30);
+    myStack.push(40);
+    myStack.push(50);
+
+    
+    myStack.print();
+
+    
+    cout << "Top element: " << myStack.peek() << endl;
+
+    
+    cout << "Popped element: " << myStack.pop() << endl;
+    cout << "Popped element: " << myStack.pop() << endl;
+
+    
+    myStack.print();
+
+    return 0;
 }
